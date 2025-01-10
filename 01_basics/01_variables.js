f8720e4c84a2bf99b60dbb5dbc5535eefb5acfbe
abhisheksaibannawar <saibannawar1994@gmail.com>
2025-01-10 17:10:56 +0000

    add variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=12334
+let accountemail="abhishek@gmail.com"
+var accountOassword="1234"
+accountCity="Jaysingpur"
+
+//  accountId=2
+
+console.log(accountId);
+
+console.table([accountId, accountemail, accountOassword, accountCity]);
